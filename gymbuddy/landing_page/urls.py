e4e6d7d398a8552ace5421b503dd08dc456ccecf
riyaan-bai <riyaanb2306@gmail.com>
2025-10-23 17:51:02 +0700

    Revisi modul Target

--- gymbuddy/landing_page/urls.py
+++ gymbuddy/landing_page/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'landing page'
+app_name = 'landing_page'
 
 urlpatterns = [
     path('', views.landing_page_view, name='landing_page'),
--- landing page/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Requirement #2, #3, #4
-def landing_page_view(request):
-    context = {}
-    return render(request, 'pages/landing_page.html', context)
