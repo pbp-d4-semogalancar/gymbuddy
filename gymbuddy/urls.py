12708b146235407e834fc4be0b319a23c2333a5e
riyaan-bai <riyaanb2306@gmail.com>
2025-10-24 01:37:18 +0700

    Fixing beberapa file html

--- gymbuddy/urls.py
+++ gymbuddy/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing_page.urls')),
-    path('auth/', include('authentication.urls')),
+    path('auth/', include('authentication.urls', namespace='authentication')),
 ]
