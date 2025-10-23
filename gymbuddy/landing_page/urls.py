7194e1b4a98a3afd1966dd645163f9753de3ad84
riyaan-bai <riyaanb2306@gmail.com>
2025-10-23 22:38:21 +0700

    perbaikan struktur file

--- gymbuddy/landing_page/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'landing_page'
-
-urlpatterns = [
-    path('', views.landing_page_view, name='landing_page'),
-]
--- gymbuddy/landing_page/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def landing_page_view(request):
-    context = {}
-    return render(request, 'landing_page.html', context)
