784a112b46b5d2339b3731cf69e33859598d118e
Wildann133 <wildanmhfdz@gmail.com>
2025-10-24 18:36:39 +0700

    merge howto

--- howto/views.py
+++ howto/views.py
@@ -2,37 +2,26 @@ from django.shortcuts import render
 from .models import Exercise
 
 def exercise_list(request):
-    # Ambil parameter filter (misalnya ?muscle=Chest)
-    muscle_filter = request.GET.get('muscle')
-    equipment_filter = request.GET.get('equipment')
-
-    # Ambil semua data latihan
+    # Ambil semua data exercise
     exercises = Exercise.objects.all()
 
-    # Terapkan filter jika ada
-    if muscle_filter:
-        exercises = exercises.filter(main_muscle__icontains=muscle_filter)
-    if equipment_filter:
-        exercises = exercises.filter(equipment__icontains=equipment_filter)
+    # Filter muscle & equipment dari parameter GET
+    selected_muscle = request.GET.get('muscle')
+    selected_equipment = request.GET.get('equipment')
 
-    # buat jadi unik terhadap nama 
-    unique_exercises = {}
-    for ex in exercises:
-        if ex.exercise_name not in unique_exercises:
-            unique_exercises[ex.exercise_name] = ex
+    if selected_muscle:
+        exercises = exercises.filter(main_muscle=selected_muscle)
+    if selected_equipment:
+        exercises = exercises.filter(equipment=selected_equipment)
 
-    exercises = unique_exercises.values()
-
-
-    # Ambil daftar otot dan alat unik
-    muscles = Exercise.objects.values_list('main_muscle', flat=True).distinct()
-    equipments = Exercise.objects.values_list('equipment', flat=True).distinct()
+    # Ambil hanya nilai unik untuk dropdown
+    muscles = Exercise.objects.values_list('main_muscle', flat=True).distinct().order_by('main_muscle')
+    equipments = Exercise.objects.values_list('equipment', flat=True).distinct().order_by('equipment')
 
     return render(request, 'howto/exercise_list.html', {
         'exercises': exercises,
         'muscles': muscles,
         'equipments': equipments,
-        'selected_muscle': muscle_filter,
-        'selected_equipment': equipment_filter,
+        'selected_muscle': selected_muscle,
+        'selected_equipment': selected_equipment,
     })
-
